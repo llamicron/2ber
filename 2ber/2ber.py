15533f4f826da72ca1a8cd8a07012210c7fbae2b
Luke Sweeney <llamicron@gmail.com>
2018-07-13 19:23:09 -0500

    Adds more front end

--- 2ber/2ber.py
+++ 2ber/2ber.py
@@ -13,10 +13,14 @@ class CustomFlask(Flask):
 
 app = CustomFlask(__name__)
 
+# Routes
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/configure')
+def configuration_page():
+    return render_template('configure.html')
 
 if __name__ == '__main__':
     app.debug = True
