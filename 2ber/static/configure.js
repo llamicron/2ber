47fbcda3cd4f7791a57f592cd9eb8f48d125d0ce
Luke Sweeney <llamicron@gmail.com>
2018-07-15 18:14:32 -0500

    Wires device display tables

--- 2ber/static/configure.js
+++ 2ber/static/configure.js
@@ -4,69 +4,61 @@ var x = new Vue({
     configurationSelect: 'create',
     newConfigurationName: '',
     slackWebhook: '',
-    "controllers": {
-      "STR116": [
-        {
-          "name": "Main STR116",
-          "address": 0
-        }
-      ],
-      "STR008": [],
-      "OmegaCN7500": [
-        {
-          "name": "Main Omega",
-          "address": 1
-        }
-      ]
-    },
-    "devices": {
-      "onOffs": [
-        {
-          "name": "HLT Valve",
-          "address": "4",
-          "controller_address": 0,
-          "state": 0
-        }
-      ],
-      "diverts": [
-        {
-          "name": "RIMS Divert",
-          "address": 3,
-          "controller_address": 0,
-          "locations": {
-            "0": "boil",
-            "1": "mash"
-          },
-          "state": 1
-        }
-      ],
-      "variables": [
-        {
-          "name": "Propane Valve",
-          "address": 2,
-          "controller_address": 0,
-          "state": 0.4
-        }
-      ],
-      "pumps": [
-        {
-          "name": "RIMS Pump",
-          "address": 1,
-          "controller_address": 0,
-          "state": 1
-        }
-      ],
-      "thermostats": [
-        {
-          "name": "RIMS Thermostat",
-          "address": 0,
-          "controller_address": 1,
-          "pv": 82.4,
-          "sv": 142.7,
-          "state": 1
-        }
-      ]
-    },
+    controllers: [
+      {
+        "name": "Main STR116",
+        "type": "STR116",
+        "address": 0
+      },
+      {
+        "name": "Main Omega",
+        "type": "OmegaCN7500",
+        "address": 1
+      }
+    ],
+    devices: [
+      {
+        "name": "HLT Valve",
+        "type": "onOff",
+        "address": "4",
+        "controller_address": 0,
+        "state": 0
+      },
+      {
+        "name": "RIMS Divert",
+        "type": "divert",
+        "address": 3,
+        "controller_address": 0,
+        "locations": {
+          "0": "boil",
+          "1": "mash"
+        },
+        "state": 1
+      },
+      {
+        "name": "Propane Valve",
+        "type": "variable",
+        "address": 2,
+        "controller_address": 0,
+        "state": 0.4
+      },
+      {
+        "name": "RIMS Pump",
+        "type": "pump",
+        "address": 1,
+        "controller_address": 0,
+        "state": 1
+      },
+      {
+        "name": "RIMS Thermostat",
+        "type": "thermostat",
+        "address": 0,
+        "controller_address": 1,
+        "pv": 82.4,
+        "sv": 142.7,
+        "state": 1
+      }
+    ],
     newController: {
       name: '',
       address: ''
@@ -75,13 +67,31 @@ var x = new Vue({
   },
 
   methods: {
+    deviceType(type) {
+      // Returns an array of devices of a vertain type
+      return this.devices.filter(x => x.type == type)
+    },
+
     addController() {
-      this.controllers[this.newController.type].push(this.newController);
+      // Pushes new controller onto the configuration model to be sent and clears newController
+      this.controllers.push(this.newController);
       this.newController = {
         name: '',
         address: ''
       }
+    },
+
+    controllerNameByAddress(adr) {
+      con = this.controllers.filter(x => x.address == adr)[0];
+      if (con) {
+        return con.name;
+      }
+      return '';
     }
+  },
+
+  computed: {
+
   },
 
   mounted() {
