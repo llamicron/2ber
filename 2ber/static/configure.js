a40cdb2aa4e163aeb91adacda282f4cdba03500d
Luke Sweeney <llamicron@gmail.com>
2018-07-15 17:48:02 -0500

    Adds a different configuration, going to undo

--- 2ber/static/configure.js
+++ 2ber/static/configure.js
@@ -4,31 +4,81 @@ var x = new Vue({
     configurationSelect: 'create',
     newConfigurationName: '',
     slackWebhook: '',
-    controllers: [
-      {
-        "name": "Main STR116",
-        "type": "STR116",
-        "address": 0
-      },
-      {
-        "name": "Main Omega",
-        "type": "OmegaCN7500",
-        "address": 1
-      }
-    ],
+    "controllers": {
+      "STR116": [
+        {
+          "name": "Main STR116",
+          "address": 0
+        }
+      ],
+      "STR008": [],
+      "OmegaCN7500": [
+        {
+          "name": "Main Omega",
+          "address": 1
+        }
+      ]
+    },
+    "devices": {
+      "onOffs": [
+        {
+          "name": "HLT Valve",
+          "address": "4",
+          "controller_address": 0,
+          "state": 0
+        }
+      ],
+      "diverts": [
+        {
+          "name": "RIMS Divert",
+          "address": 3,
+          "controller_address": 0,
+          "locations": {
+            "0": "boil",
+            "1": "mash"
+          },
+          "state": 1
+        }
+      ],
+      "variables": [
+        {
+          "name": "Propane Valve",
+          "address": 2,
+          "controller_address": 0,
+          "state": 0.4
+        }
+      ],
+      "pumps": [
+        {
+          "name": "RIMS Pump",
+          "address": 1,
+          "controller_address": 0,
+          "state": 1
+        }
+      ],
+      "thermostats": [
+        {
+          "name": "RIMS Thermostat",
+          "address": 0,
+          "controller_address": 1,
+          "pv": 82.4,
+          "sv": 142.7,
+          "state": 1
+        }
+      ]
+    },
     newController: {
       name: '',
-      type: '',
       address: ''
-    }
+    },
+
   },
 
   methods: {
     addController() {
-      this.controllers.push(this.newController);
+      this.controllers[this.newController.type].push(this.newController);
       this.newController = {
         name: '',
-        type: '',
         address: ''
       }
     }
